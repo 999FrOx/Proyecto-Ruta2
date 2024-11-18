4d40020e29ff6c79406ee4e8920f554860e45b8f
999FrOxgg <luispgonz@outlook.com>
2024-11-18 15:33:28 -0600

    ProyectoRuta2

--- src/RutaVista.java
+++ src/RutaVista.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RutaVista {
     private JPanel pPrincipal;
     private JTextField tfNoUnidad;
-    private JTextField tfPlacas;
+    private JTextField tfMatricula;
     private JButton btAgregar;
+    private List<Unidad> unidades;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("RutaVista");
@@ -15,14 +18,21 @@ public class RutaVista {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(500, 500);
     }
 
     public RutaVista() {
+        unidades = new ArrayList<>();
         btAgregar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                Unidad unidad = new Unidad();
+                unidad.setNoUnidad(Integer.parseInt(tfNoUnidad.getText()));
+                unidad.setMatricula(tfMatricula.getText());
+                    unidades.add(unidad);
             }
+
         });
     }
 }
--- src/Unidad.java
+++ src/Unidad.java
@@ -1,14 +1,39 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Unidad {
-    private String noUnidad, placa;
+    private int noUnidad;
+    private String matricula;
+    private List<String> listaUnidad;
 
     public Unidad() {
         this.noUnidad = noUnidad;
-        this.placa = placa;
-    }
-    LinkedList<String> listaUnidad = new LinkedList<>();
-    public void agregar(){
-        listaUnidad.add(elemento);
+        this.matricula = matricula;
+        this.listaUnidad = new ArrayList<>();
     }
 
+    public int getNoUnidad() {
+        return noUnidad;
+    }
+
+    public void setNoUnidad(int noUnidad) {
+        this.noUnidad = noUnidad;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public List<String> getListaUnidad() {
+        return listaUnidad;
+    }
+
+    @Override
+    public String toString() {
+        return "|Unidad " + noUnidad + "|" + " |Matricula= " + matricula + "|";
+    }
 }
