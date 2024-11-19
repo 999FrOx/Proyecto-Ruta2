4b6f3fdf7210278e3cbf71d96f3c361625a1f659
999FrOxgg <luispgonz@outlook.com>
2024-11-18 20:14:02 -0600

    ProyectoRuta2

--- src/RutaVista.java
+++ src/RutaVista.java
@@ -10,6 +10,8 @@ public class RutaVista {
     private JTextField tfNoUnidad;
     private JTextField tfMatricula;
     private JButton btAgregar;
+    private JComboBox cbUnidades;
+    private JButton quitarUnidadButton;
     private List<Unidad> unidades;
 
     public static void main(String[] args) {
@@ -18,8 +20,9 @@ public class RutaVista {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
         frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+
     }
 
     public RutaVista() {
@@ -27,12 +30,70 @@ public class RutaVista {
         btAgregar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 Unidad unidad = new Unidad();
+                if (tfNoUnidad.getText().isEmpty() || tfMatricula.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Por favor, complete todos los campos.", "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
                 unidad.setNoUnidad(Integer.parseInt(tfNoUnidad.getText()));
                 unidad.setMatricula(tfMatricula.getText());
-                    unidades.add(unidad);
+                unidades.add(unidad);
+                agregarUn();
+                tfNoUnidad.setText("");
+                tfMatricula.setText("");
+
+            }
+            //Agregar unidades para atender
+            public void agregarUn() {
+                if (!unidades.isEmpty()) {
+                    Unidad ultUnidad = unidades.get(unidades.size() - 1);
+                    cbUnidades.addItem(ultUnidad);
+                }
+            }
+        });
+        //Boton para quitar unidades
+        quitarUnidadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String unidadEl = JOptionPane.showInputDialog("Ingrese el número de unidad a eliminar:");
+                    if (unidadEl == null || unidadEl.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Llene los datos", "Error", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+                    int numeroUnidadEl = Integer.parseInt(unidadEl);
+                    boolean eliminada = eliminarUnidad(numeroUnidadEl);
+
+                    if (eliminada) {
+                        JOptionPane.showMessageDialog(null, "La unidad fue eliminada con éxito.");
+                        actualizarOpciones();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "La unidad no existe.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
             }
 
+            //Compara si el numero ingresado esta en el arreglo
+            private boolean eliminarUnidad(int numeroUnidad) {
+                for (Unidad unidad : unidades) {
+                    if (unidad.getNoUnidad() == numeroUnidad) {
+                        unidades.remove(unidad);
+                        return true;
+                    }
+                }
+                return false;
+            }
+            //Actualiza en JCombobox
+            public void actualizarOpciones() {
+                cbUnidades.removeAllItems();
+                for (Unidad unidad : unidades) {
+                    cbUnidades.addItem(unidad);
+                }
+            }
         });
+
     }
 }
